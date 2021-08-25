d2a9001017a6e43dc165564b3f6311f34aedd0df
ansansaram <ansansaram_@naver.com>
2021-08-25 22:27:53 +0900

    1929_소수 구하기.cpp

---
+++ CLASS 2/1929_소수 구하기.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int M, N;
+	cin >> M >> N;
+
+	int *X = new int[N + 1]{ 0 };
+
+	for (int i = 2; i < N+1; i++)
+	{
+		X[i] = i;
+	}
+	for (int i = 2; i < N+1; i++)
+	{
+		if (X[i]==0)
+		{
+			continue;
+		}
+		else
+		{
+			for (int j = 2*i; j < N+1; j+=i)
+			{
+				X[j] = 0;
+			}
+		}
+	}
+	for (int i = M; i <=N; i++)
+	{
+		if (X[i]!=0)
+		{
+			cout << X[i] << "\n";
+		}
+	}
+
+
+	return 0;
+}
