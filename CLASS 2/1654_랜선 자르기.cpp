26de9972ac51de93d598556a9efe06397c511d56
ansansaram <ansansaram_@naver.com>
2021-08-27 00:57:55 +0900

    1654_랜선 자르기.cpp
    시간 초과.
    이진 탐색으로 풀어야 함.

--- CLASS 2/1654_랜선 자르기.cpp
+++ CLASS 2/1654_랜선 자르기.cpp
@@ -1,3 +1,9 @@
+/*
+    틀린 풀이. 갖고 있는 랜선 중 최대길이 부터 시작하여 몫을 다 더한 것이 필요한 랜선 N보다 큰지 비교하는 방식
+    선형 탐색이라 시간초과 난다. 탐색 시간 log(N)인 이진탐색으로 풀어야 한다.
+    또한, 입력값이 0 ~ 2^31 - 1이므로 길이 변수들은 long long으로 선언하자.
+*/
+
 #include<iostream>
 using namespace std;
 
@@ -10,16 +16,16 @@ int main(){
     int K,N;
     cin>>K>>N;
     
-    int totalLength = 0;
-
+    int maxLength=0;
     for (int i = 0; i < K; i++)
     {
         cin>>arr[i];
-        totalLength+=arr[i];
+        if (maxLength<arr[i])
+        {
+            maxLength=arr[i];
+        }
+        
     }
-
-    int maxLength = totalLength/N;
-
     while (true)
     {
         int shareSum = 0;
