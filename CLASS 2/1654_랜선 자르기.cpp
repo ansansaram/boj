dbdd8c0dbafe1895e6afddd3a51070f8084c8f9a
ansansaram <ansansaram_@naver.com>
2021-08-27 01:19:57 +0900

    Update 1654_랜선 자르기.cpp
    선형탐색X -> 이진탐색O
    무한루프 조심

--- CLASS 2/1654_랜선 자르기.cpp
+++ CLASS 2/1654_랜선 자르기.cpp
@@ -1,42 +1,54 @@
-/*
-    틀린 풀이. 갖고 있는 랜선 중 최대길이 부터 시작하여 몫을 다 더한 것이 필요한 랜선 N보다 큰지 비교하는 방식
-    선형 탐색이라 시간초과 난다. 탐색 시간 log(N)인 이진탐색으로 풀어야 한다.
-    또한, 입력값이 0 ~ 2^31 - 1이므로 길이 변수들은 long long으로 선언하자.
-*/
-
 #include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
 
-int arr[10001] = {0,};
+typedef long long ll;
+
+ll arr[10001] = {0};
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int K,N;
-    cin>>K>>N;
-    
-    int maxLength=0;
+    int K, N;
+    cin >> K >> N;
+
+    ll maxLength = 0; // N개를 만들 수 있는 랜선의 최대 길이
+
+    ll maxInput = 0; // 입력 받은 랜선 중 최대 길이
     for (int i = 0; i < K; i++)
     {
-        cin>>arr[i];
-        if (maxLength<arr[i])
+        cin >> arr[i];
+        if (maxInput <= arr[i])
         {
-            maxLength=arr[i];
+            maxInput = arr[i];
         }
-        
-    }
-    while (true)
-    {
-        int shareSum = 0;
-        for (int i = 0; i < K; i++)
-        {
-            shareSum += arr[i]/maxLength;
-        }
-        
-        if(shareSum == N) {cout<< maxLength; break;}
-        else maxLength--;
     }
     
+    ll low = 1;
+    ll high = maxInput;
+
+    while (low<=high)
+    {
+        ll mid = (low + high) / 2;
+        int sum = 0;
+        for (int i = 0; i < K; i++)
+        {
+            sum += arr[i]/mid; // sum : 길이 mid로 자를 때 랜선의 개수
+        }
+        if (sum >= N) // 길이 mid 짜리 랜선을 N개 만들 수 있는 경우
+        {             
+            if (maxLength < mid) // 현재까지의 최대길이보다 mid가 길면 Update
+            {
+                maxLength = mid;
+            }
+            low = mid + 1; // mid 보다 큰 범위에서 재탐색
+        }
+        else high = mid - 1; // 만들 수 없는 경우 mid 보다 작은 범위에서 재탐색
+    }
+    
+    cout << maxLength;
+
     return 0;
 }
