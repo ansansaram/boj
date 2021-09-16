20636566036c0b2c9385be6fbee2507808ed6e28
ansansaram <ansansaram_@naver.com>
2021-09-16 20:03:11 +0900

    Update 5525_IOIOI.cpp
    이전 방법은 일일이 비교해서 시간복잡도가 O(M^2)이었다. 두 번째 태스크에서 시간초과났다. 지금 방법으로 시간복잡도 O(M)에 해결가능하다.

--- CLASS 3/5525_IOIOI.cpp
+++ CLASS 3/5525_IOIOI.cpp
@@ -1,50 +1,56 @@
+/*
+    O(M)에 푸는 방법
+    I가 나올때 다음, 다다음 문자가 O, I 이면 k를 1 증가시켜준다.
+    k = 1 => IOI
+    k = 2 => IOIOI .. 
+    
+    인덱스는 i -> i+2로 바꾸어서 탐색한다.
+
+
+    k = N 이라면 찾는 문자열이 있다는 의미이다.
+    이때 총 cnt를 1 증가 시켜주고
+    k를 1 감소 시켜준다.(이전의 문자열 중복 제거 위해서)
+*/
+
 #include<iostream>
-#include<string>
-#include<vector>
+
 using namespace std;
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-
-    vector<char> S;
-    
     int N, M;
     cin >> N >> M;
 
-    string p;
-
-    p = "I";
-    for (int i = 0; i < N; i++)
-    {
-        p+="OI";
-    }
-    
+    string s;
+    cin >> s;
+    int cnt = 0;
     for (int i = 0; i < M; i++)
     {
-        char temp;
-        cin >> temp;
-        S.push_back(temp);
+        
+        if (s[i]=='O')
+        {
+            continue;
+        }
+        else
+        {
+            int k = 0;
+            while (s[i+1]=='O'&&s[i+2]=='I')
+            {
+                k++; // k가 1 이면 IOI까지 찾았다는 의미
+                if (k == N) // 원하는 문자열 찾았으면
+                {
+                    cnt++; // 카운트 +1 해주고
+                    k--; // 뒤에 또 OI나올 수 있으므로 중복 제거 위해 k 1감소시킨다.
+                    // IOIOI라는 문자열에서 IOI를 찾을 때
+                    // 앞의 IOI발견했을때 카운트 +1해주고 앞의 IO부분은 다시 세면 안되므로 -1 해주는 것
+                }
+                i+=2;
+            }
+        }
     }
     
-    int cnt = 0;
-    string cmp;
-    for (int i = 0; i < M-(2*N+1); i++)
-    {
-        cmp="";
-        for (int j = i; j < i + (2*N+1); j++)
-        {
-            cmp+=S[j];
-        }
-        if (cmp == p)
-        {
-            cnt++;
-        }
-    }
-    
-    cout <<cnt;
-
-    
+    cout << cnt;
 
     return 0;
 }
