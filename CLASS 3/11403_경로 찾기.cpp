dc953f186d27c50c0075117b1510d1e29027f42c
ansansaram <ansansaram_@naver.com>
2021-09-21 20:11:48 +0900

    11403_경로 찾기.cpp
    Floyd-Warshall Algorithm

---
+++ CLASS 3/11403_경로 찾기.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+int arr[100][100] = {0, };
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    // Floyd-Warshall Algorithm
+    for (int k = 0; k < N; k++)
+    {
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                if (arr[i][k] && arr[k][j])
+                {
+                    arr[i][j] = 1;
+                }
+            }
+        }
+    }
+    
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
