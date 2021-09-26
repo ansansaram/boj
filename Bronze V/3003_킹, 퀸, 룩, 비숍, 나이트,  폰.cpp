ba7a00044210f564f232a0f02acbb4c1990b1990
ansansaram <ansansaram_@naver.com>
2021-09-27 00:53:39 +0900

    3003_킹, 퀸, 룩, 비숍, 나이트, 폰.cpp

---
+++ Bronze V/3003_킹, 퀸, 룩, 비숍, 나이트,  폰.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int chess[6] = {1,1,2,2,2,8};
+    for (int i = 0; i < 6; i++)
+    {
+        int temp;
+        cin >> temp;
+        chess[i] -= temp;
+    }
+    for (int i = 0; i < 6; i++)
+    {
+        cout << chess[i] << " ";
+    }
+    
+    
+    return 0;
+}
