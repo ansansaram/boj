86f798920d9ab47d7008e6ce5795bb41e61fecea
ansansaram <ansansaram_@naver.com>
2021-10-15 01:01:53 +0900

    14645_와이버스 부릉부릉.cpp

---
+++ Bronze V/14645_와이버스 부릉부릉.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int N, K;
+    cin >> N >> K;
+
+    while (N--)
+    {
+        int A , B;
+        cin >> A >> B;
+    }
+    
+    cout << "비와이";
+    
+    return 0;
+}
